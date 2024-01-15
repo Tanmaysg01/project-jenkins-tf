c4ae005103658a67fdbf5ada51406d5d269c45f8
Tanmay Garge <tanmaygarge@gmail.com>
2024-01-15 15:17:03 +0530

    s3

--- s3.tf
+++ s3.tf
@@ -6,14 +6,3 @@ resource "aws_s3_bucket_notification" "s3_bucket_eventbridge" {
   bucket      = "projectbucket001"
   eventbridge = true
 }
-
-resource "aws_s3_bucket" "webhook" {
-  bucket = "webhook-bucket-jenkins-project"
-}
-
-resource "aws_s3_bucket_object" "object" {
-  bucket = "event-pipeline-trigger-bucket"
-  key    = "Book1.csv"
-  acl    = "private"
-  source = "https://event-pipeline-trigger-bucket.s3.amazonaws.com/Book1.csv"
-}
