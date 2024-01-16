9ad645894bd1d98fbaf8bd1db1fcc5d333ced6c4
Tanmay Garge <tanmaygarge@gmail.com>
2024-01-16 11:06:41 +0530

    s3

--- s3.tf
+++ s3.tf
@@ -10,3 +10,7 @@ resource "aws_s3_bucket_notification" "s3_bucket_eventbridge" {
 resource "aws_s3_bucket" "example" {
   bucket = "my-jenkins-test-bucket"
 }
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "automation-test-bucket"
+}
