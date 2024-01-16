da971b082da0b469a2d7cf3a254d53624135a9c7
Tanmay Garge <tanmaygarge@gmail.com>
2024-01-16 10:26:47 +0530

    s3

--- s3.tf
+++ s3.tf
@@ -6,12 +6,3 @@ resource "aws_s3_bucket_notification" "s3_bucket_eventbridge" {
   bucket      = "projectbucket001"
   eventbridge = true
 }
-
-
-resource "aws_s3_bucket" "example2" {
-  bucket = "event-project-bucket"
-}
-
-resource "aws_s3_bucket" "example1" {
-  bucket = "webhook-bucket-try"
-}
