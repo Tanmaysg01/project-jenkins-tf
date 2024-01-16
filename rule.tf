f5f3d11dce50545c8b1d221ccdb3e67e73f05a1d
Tanmay Garge <tanmaygarge@gmail.com>
2024-01-16 10:40:07 +0530

    rule

--- rule.tf
+++ rule.tf
@@ -1,33 +1,92 @@
-resource "aws_cloudwatch_event_rule" "s3eventrule" {
-  name        = "s3eventrule"
-  description = "Event rule for CSV file uploaded to S3 bucket"
+resource "aws_iam_role" "event_role" {
+  name = "eventbridge_invoke_lambda_role"
 
-  event_pattern = <<EOF
+  assume_role_policy = <<EOF
 {
-  "detail": {
-    "object": {
-      "key": [
-        {
-          "suffix": ".csv"
-        }
-      ]
-    },
-    "bucket": {
-      "name": [
-        "${aws_s3_bucket.bucket.id}"
-      ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      }
     }
-  },
-  "detail-type": ["Object Created"],
+  ]
+}
+EOF
+}
+resource "aws_iam_policy" "event_policy" {
+  name        = "event_rule_policy"
+  description = "IAM policy for EventBridge rule to invoke Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "lambda:InvokeFunction",
+        ],
+        Effect   = "Allow",
+        Resource = aws_lambda_function.test_lambda.arn,
+      },
+    ],
+  })
+}
+resource "aws_iam_role_policy_attachment" "event-attach" {  
+  policy_arn = aws_iam_policy.event_policy.arn
+  role       = aws_iam_role.event_role.name
+  depends_on = [
+    aws_iam_role.event_role,
+    aws_iam_policy.event_policy,
+  ]
+ }
+resource "aws_cloudwatch_event_rule" "s3eventrule" {
+  name        = "project_rule"
+  description = "CloudWatch Event Rule for S3 API Calls"
+  role_arn = aws_iam_role.event_role.arn
+
+event_pattern = <<EOF
+{
   "source": ["aws.s3"]
 }
 EOF
 }
+# resource "aws_cloudwatch_event_rule" "s3eventrule" {
+#   name        = "s3eventrule"
+#   description = "Capture each AWS Console Sign In"
 
-resource "aws_cloudwatch_event_target" "s3_event_target" {
-  rule      = aws_cloudwatch_event_rule.s3eventrule.name
-  target_id = "s3_event_target"
-  arn       = aws_lambda_function.test_lambda.arn
+#   event_pattern = jsonencode({
+#   detail_type = ["Object Created", "Object Deleted"],
+#   source = ["aws.s3"],
+#   detail = {
+#     bucket = {
+#     bucketName = [aws_s3_bucket.bucket.bucket],  
+#     },
+#    },
+# })
+# }
 
-  depends_on = [aws_cloudwatch_event_rule.s3eventrule]
+
+# resource "aws_cloudwatch_event_target" "lambda_target" {
+#   rule = aws_cloudwatch_event_rule.s3eventrule.name
+#   arn  = aws_lambda_function.test_lambda.arn
+#   role_arn = aws_iam_role.event_role.arn
+
+# }
+
+resource "aws_cloudwatch_event_target" "event_target" {
+    rule = aws_cloudwatch_event_rule.s3eventrule.name
+    target_id = "test_lambda"
+    arn = aws_lambda_function.test_lambda.arn
+    
+
+}
+
+resource "aws_lambda_permission" "lambdapermission" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.test_lambda.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.s3eventrule.arn
 }
